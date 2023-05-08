8dfda82c6ff3e19b3a1e9dac53f38050173e05d3
abragtim <timurkatime@gmail.com>
2023-05-08 16:36:40 +0200

    Add calculation for flour.

--- src/main/java/com/cooking/calculator/CalculatorApplication.java
+++ src/main/java/com/cooking/calculator/CalculatorApplication.java
@@ -2,12 +2,25 @@ package com.cooking.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 
 @SpringBootApplication
+@RestController
 public class CalculatorApplication {
+
+	private static final float flour_grams_in_tbsp = 30.3f;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
 
+	@GetMapping("/flour-to-tbsp")
+	public List<Integer> flour_grams_to_tbsp(@RequestParam(value = "grams") int grams) {
+		return List.of((int) (grams/flour_grams_in_tbsp));
+	}
 }
